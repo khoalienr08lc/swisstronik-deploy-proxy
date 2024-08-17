d884c5de2c9b445708978da87f48ec2f812367ba
khoalienr08lc <khoalienr08lc@gmail.com>
2024-08-17 21:11:33 +0000

    quest

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xdBe9E844D9f6b34a11AcA359Fa7998882c29348c'
+export const ProxyAdmin = '0x57318E93702dfc2071A30D2A2254805573047928'
+export const SWTRImplementation = '0x0EDa1933B514D7E93460aAbB75237839e0518b7f'
     
